25b239cb11adf70305fc8da35d9e2338bd97e62b
shripalrp05 <shripalrp05@gmail.com>
2026-08-12 00:40:30 +0530

    Added API Test for search products

--- tests/products/api/productApiTest.spec.ts
+++ tests/products/api/productApiTest.spec.ts
@@ -18,3 +18,20 @@ test('GET /api/productsList returns a non-empty product list', {
   });
 });
 
+test('Search Products using a search term.', {
+  tag: ['@smoke', '@api'],
+}, async ({ productService }) => {
+  const products = await productService.searchProducts('top');
+
+  expect(products.length).toBeGreaterThan(0);
+
+  const firstProduct = products[0];
+  expect(firstProduct).toEqual({
+    id: expect.any(Number),
+    name: expect.stringMatching(/top/i),
+    price: expect.any(Number),
+    brand: expect.any(String),
+    category: expect.any(String),
+    userType: expect.any(String)
+  });
+});
